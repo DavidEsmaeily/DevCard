d71212d3c683485d661e093069868908f0d34111
DavidEsmaeily <DavoodEsmaeily0049@gmail.com>
2021-12-19 13:14:48 +0330

    Index & Contact page add to project

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
